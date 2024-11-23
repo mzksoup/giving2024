d91f31de4a810a80c2a439c90013999bd723ba47
mzksoup <sei_r+git@addor.jp>
2024-11-23 20:56:19 +0900

    update-items

--- js/items.js
+++ js/items.js
@@ -3,45 +3,105 @@ export const items = [
     id: 1,
     name: "【チャリティーカレンダー】季節の富士山（2025年1月始まりカレンダー）",
     price: 1650,
-    description: "説明1",
+    description:
+      "四季折々の富士山を切り取ったカレンダー。毎月異なる絶景が、あなたの日常に彩りを添えます。\n【ハゴロモチャリティについて】社会貢献活動につながる寄付を目的としたチャリティ商品です。このカレンダーの売上の一部を寄付いたします。",
     image: "https://shop.r10s.jp/book/cabinet/6508/4900459556508_1_3.jpg",
-    tags: ["#タグ1", "#タグ2"],
-    isNew: true,
-    purchaseUrl: "https://a.r10.to/hPzRBx", // 販売ページのURL
+    tags: ["#カレンダー"],
+    isNew: false,
+    purchaseUrl: "https://a.r10.to/hPzRBx",
   },
   {
     id: 2,
     name: "【チャリティーカレンダー】卓上 くつろぎねこ（2025年1月始まりカレンダー）",
     price: 1210,
-    description: "説明2",
+    description:
+      "くつろいでいる猫の写真が掲載されていて、見ているだけで癒される猫好きにはたまらないカレンダーです。玄関や寝室などに置くとその空間が和やかになります。\n【ハゴロモチャリティについて】社会貢献活動につながる寄付を目的としたチャリティ商品です。このカレンダーの売上の一部を寄付いたします。",
     image: "https://shop.r10s.jp/book/cabinet/6478/4900459556478_1_3.jpg",
-    tags: ["#ハガキ", "#動物愛護"],
+    tags: ["#カレンダー"],
     isNew: false,
-    purchaseUrl: "https://a.r10.to/hk0jHB", // 販売ページのURL
+    purchaseUrl: "https://a.r10.to/hk0jHB",
   },
   {
     id: 3,
     name: "チャリティカレンダー2025「いま、何時？」",
     price: 1200,
     description:
-      "移住連が、日本に暮らす移民・難民、移民ルーツをもつ人びとの姿を伝え、エンパワメントすることを目的として、毎年発行しているカレンダー",
+      "移住連が、日本に暮らす移民・難民、移民ルーツをもつ人びとの姿を伝え、エンパワメントすることを目的として、毎年発行しているカレンダーのご案内です。",
     image:
       "https://baseec-img-mng.akamaized.net/images/item/origin/ae850c5cabb11b6570b9d63b334f5140.png",
-    tags: ["#カレンダー", "#教育支援"],
+    tags: ["#カレンダー", "#国際協力・人道支援"],
     isNew: false,
-    purchaseUrl: "https://migrants.jp/news/office/20240924_calendar.html", // 販売ページのURL
+    purchaseUrl: "https://migrants.jp/news/office/20240924_calendar.html",
   },
   {
     id: 4,
     name: "2025ラジオ大阪「もう、好きにやります。」カレンダー",
     price: 1200,
-    description: "説明4",
+    description:
+      "出演者が「もう、好き」に書いたメッセージ入りの2025年チャリティーカレンダー。製作費・経費を除く全額がラジオ・チャリティ・ミュージックソンの募金となり、目の不自由な方の生活に役立てられます。",
     image:
       "https://item-shopping.c.yimg.jp/i/n/radioosaka_cal2025-001_i_20241030155423",
-    tags: ["#カレンダー", "#環境保護"],
+    tags: ["#カレンダー", "#福祉・障がい"],
     isNew: false,
     purchaseUrl:
-      "https://store.shopping.yahoo.co.jp/radioosaka/cal2025-001.html", // 販売ページのURL
+      "https://store.shopping.yahoo.co.jp/radioosaka/cal2025-001.html",
+  },
+  {
+    id: 5,
+    name: "【数量限定】2025年壁掛けカレンダー ちはやふる基金チャリティー [chihaya90005]",
+    price: 3800,
+    description:
+      "末次由紀による『ちはやふる』描き下ろしイラストをふんだんに使用した、2025年1月〜12月にお使いいただける壁掛けカレンダーです。",
+    image:
+      "https://imagedelivery.net/QondspN4HIUvB_R16-ddAQ/5e12ae0a7f28e90bbd7801f1/5440b7282b196b76fd7e.png/original",
+    tags: ["#カレンダー", "#アート・文化"],
+    isNew: true,
+    purchaseUrl: "https://chihayafund.stores.jp/items/66e6401a16b7bc0aa1e34ca7",
+  },
+  {
+    id: 6,
+    name: "【数量限定】2025年卓上カレンダー ちはやふる基金チャリティー [chihaya90006]",
+    price: 2700,
+    description:
+      "末次由紀による『ちはやふる』完全描き下ろしイラストをふんだんに使用した、2025年1月〜12月にお使いいただける卓上カレンダーです。",
+    image:
+      "https://imagedelivery.net/QondspN4HIUvB_R16-ddAQ/5e12ae0a7f28e90bbd7801f1/332915d6944cd139f798.png/original",
+    tags: ["#カレンダー", "#アート・文化"],
+    isNew: true,
+    purchaseUrl: "https://chihayafund.stores.jp/items/66e64191155f2d2fd3319b09",
+  },
+  {
+    id: 7,
+    name: "【数量限定】2025年カレンダーセット ちはやふる基金チャリティー [chihaya90004]",
+    price: 5500,
+    description: "カレンダーをそれぞれ個別で買うより1,000円お得なセットです。",
+    image:
+      "https://imagedelivery.net/QondspN4HIUvB_R16-ddAQ/5e12ae0a7f28e90bbd7801f1/8126ce721f4d4f88169b.png/original",
+    tags: ["#カレンダー", "#アート・文化"],
+    isNew: true,
+    purchaseUrl: "https://chihayafund.stores.jp/items/66e63f1c2bd35e45be4a2d6c",
+  },
+  {
+    id: 8,
+    name: "JHPチャリティーカレンダー「Smile Moment」",
+    price: 500,
+    description:
+      "毎年好評いただいているJHPチャリティーカレンダー「Smile Moment」。今年もカンボジア・ネパールの現地の子どもたちの溢れる笑顔がつまったカレンダーが出来上がりました。",
+    image: "http://www.jhp.or.jp/img/onlinenews/2024/calendar2025/omote.jpg",
+    tags: ["#カレンダー", "#国際協力・人道支援"],
+    isNew: false,
+    purchaseUrl: "https://www.jhp.or.jp/2024/10/2025_jhp_calender/",
+  },
+  {
+    id: 9,
+    name: "ワールドチルドレン芸術祭2025カレンダー",
+    price: 1000,
+    description:
+      "2025年のワールドチルドレン芸術祭チャリティカレンダーを製作しております。2025年のテーマは「あなたにとっての宝物」。",
+    image: "https://pwshop.ocnk.net/data/pwshop/product/20241004_29d166.JPG",
+    tags: ["#カレンダー", "#国際協力・人道支援"],
+    isNew: false,
+    purchaseUrl: "https://www.worldchildren-smile.com/charity-calendar2024",
   },
 ];
 
